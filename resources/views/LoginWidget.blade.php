b94d6e929b62d1a22853fcb2552cc8213570b0d8
ahmadrifai <ahmatrifai8721@gmail.com>
2024-05-23 08:34:17 +0700

    updatea view

--- resources/views/LoginWidget.blade.php
+++ resources/views/LoginWidget.blade.php
@@ -1,6 +1,20 @@
 {{-- <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot"
     data-size="medium" data-radius="14" data-auth-url="" data-request-access="write"></script> --}}
 
+<!DOCTYPE html>
+<html lang="en">
 
-<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot" data-size="large"
-    data-auth-url="https://webhook.koalaterbang.cloud/webhookLogin"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+
+<body>
+    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot"
+        data-size="large" data-auth-url="https://webhook.koalaterbang.cloud/webhookLogin"></script>
+
+</body>
+
+</html>
