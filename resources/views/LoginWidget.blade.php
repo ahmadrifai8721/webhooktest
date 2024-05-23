7ff7347a80b15715774099454e1fa09750207834
ahmadrifai <ahmatrifai8721@gmail.com>
2024-05-23 08:31:26 +0700

    p

--- resources/views/LoginWidget.blade.php
+++ resources/views/LoginWidget.blade.php
@@ -1,2 +1,6 @@
-<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot" data-size="medium"
-    data-radius="14" data-auth-url="" data-request-access="write"></script>
+{{-- <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot"
+    data-size="medium" data-radius="14" data-auth-url="" data-request-access="write"></script> --}}
+
+
+<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="KoalaTeam_bot" data-size="large"
+    data-auth-url="https://webhook.koalaterbang.cloud/webhookLogin"></script>
